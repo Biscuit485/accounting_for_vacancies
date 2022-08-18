92ee537673e6013cf170efffd4ea1bd7594adeb9
Danil Ostroverkh <ostroverh@list.ru>
2022-08-18 16:27:31 +0500

    Initial commit

---
+++ src/test/java/com/company/accounting_for_vacancies/AccountingForVacanciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.accounting_for_vacancies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingForVacanciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
